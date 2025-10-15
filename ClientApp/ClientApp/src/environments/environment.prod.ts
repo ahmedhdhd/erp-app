e2bd72310c82e0d787154b760ebe42a4bcb4485f
ahmedhdhd <ahmedhamed6122001@gmail.com>
2025-10-15 19:06:47 +0100

    add prod

---
+++ ClientApp/ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// This file is used for production builds
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://erpappservice.azurewebsites.net/api',
+  appName: 'ERP System',
+  version: '1.0.0'
+};
