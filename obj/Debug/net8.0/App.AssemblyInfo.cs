7734711d154683b2d546da4566e8961e7652b573
ahmedhdhd <ahmedhamed6122001@gmail.com>
2025-10-22 12:43:42 +0100

    full

--- obj/Debug/net8.0/App.AssemblyInfo.cs
+++ obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83199b72b37082582fafa89263ae9a30f9c3f397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+134cc235cb40a0ed6718c0be43b5a8a4c311e753")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
